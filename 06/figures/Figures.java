337cffbe78cb418970a5bf19b797ac294b11dc63
Pedro Lucas <92189406+BitGigbits@users.noreply.github.com>
2022-03-20 20:33:40 -0300

    Add files via upload

--- 06/figures/Figures.java
+++ 06/figures/Figures.java
@@ -2,5 +2,28 @@ package figures;
 import java.awt.*;
 
 public abstract class Figures {
+    public int x, y;
+    public int rc, gc, bc;
+    public int ri, gi, bi;
+
+    public Figures(int x, int y, int rc, int gc, int bc, int ri, int gi, int bi){
+        this.x = x;
+        this.y = y;
+        this.rc = rc;
+        this.gc = gc;
+        this.bc = bc;
+        this.ri = ri;
+        this.gi = gi;
+        this.bi = bi;
+    }
+
+    public Figures(int rc, int gc, int bc, int ri, int gi, int bi){
+        this.rc = rc;
+        this.gc = gc;
+        this.bc = bc;
+        this.ri = ri;
+        this.gi = gi;
+        this.bi = bi;
+    }
     public abstract void paint (Graphics g);
 }
--- 06/figures/Rect.java
+++ 06/figures/Rect.java
@@ -2,26 +2,12 @@ package figures;
 import java.awt.*;
 
 public class Rect extends Figures{
-    private int x, y;
     private int w, h;
-    private int rc, gc, bc; //c -> Contorno
-    private int ri, gi, bi; //i -> Interno
 
     public Rect (int x, int y, int w, int h, int rc, int gc, int bc, int ri, int gi, int bi){
-        this.x = x;
-        this.y = y;
+        super(x, y, rc, gc, bc, ri, gi, bi);
         this.w = w;
         this.h = h;
-        this.rc = rc;
-        this.gc = gc;
-        this.bc = bc;
-        this.ri = ri;
-        this.gi = gi;
-        this.bi = bi;
-    }
-
-    public void print (){
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n", this.w, this.h, this.x, this.y);
     }
 
     public void paint (Graphics g){
